20d0eb7ff5a53de284c0e55e585763c70cc8a34b
Jordan Mora Kuebler <jordanmorakuebler@gmail.com>
2021-01-21 20:31:02 -0300

    Removed fields from Person
    * Removed fields name and last_name from the Person model

--- persons/models.py
+++ persons/models.py
@@ -2,14 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Person(models.Model):
-    name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
-    address = models.TextField()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.TextField()
 
     def __str__(self):
-        return self.fullname()
+        return self.fullname
 
     @property
     def fullname(self):
-        return f"{self.name} {self.last_name}"
+        return f"{self.user.get_full_name()}"
